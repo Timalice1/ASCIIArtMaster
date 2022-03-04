c848478cd44cdd7ea1fcf559dc4ffa201011adf2
Timalice1 <newacccc2@gmail.com>
2022-03-04 15:38:30 +0200

    Convert img to gray scale

---
+++ ASCIIArtMaster/Extentions.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace ASCIIArtMaster {
+    public static class Extentions {
+        public static void ToGrayScale(this Bitmap bitmap) {
+            /**
+             * Convert bitmaps pixel colors to gray scale
+             * Find the average of red, green & blue value and replace them with average value
+             */
+            for (int x = 0; x < bitmap.Width; x++) {
+                for(int y = 0; y < bitmap.Height; y++) {
+                    var pixel = bitmap.GetPixel(x, y);
+                    int avg = (pixel.R+pixel.B + pixel.G)/3;
+                    bitmap.SetPixel(x,y, Color.FromArgb(pixel.A, avg, avg, avg));
+                }
+            }
+        }
+    }
+}
--- ASCIIArtMaster/Program.cs
+++ ASCIIArtMaster/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace ASCIIArtMaster {
     internal class Program {
+
+        [STAThread]
         static void Main(string[] args) {
+
+            var openFileDilaog = new OpenFileDialog {
+                Filter = "Images | *.bmp; *.png; *.jpeg; *.JPEG; *.jpg"
+            };
+
+            while (true) {
+                Console.ReadLine();
+
+                if (openFileDilaog.ShowDialog() != DialogResult.OK)
+                    continue;
+
+                Console.Clear();
+
+                var bitmap = new Bitmap(openFileDilaog.FileName);
+                bitmap = Resize(bitmap);
+                bitmap.ToGrayScale();
+
+            }
+        }
+
+        private static Bitmap Resize(Bitmap bitmap) {
+            var maxWidth = 350;
+            var newHeight = bitmap.Height/1.5 * maxWidth/bitmap.Width;
+            if (bitmap.Width > maxWidth || bitmap.Height > newHeight)
+                bitmap = new Bitmap(bitmap, new Size(maxWidth, (int)newHeight));
+            return bitmap;
         }
     }
 }
