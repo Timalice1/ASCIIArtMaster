5d5dcb2d8ab8b3a94cbe2b7a51b5ecd53edec877
Timalice1 <newacccc2@gmail.com>
2022-03-04 16:57:06 +0200

    add converter class

---
+++ ASCIIArtMaster/BitmapToASCIIConverter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCIIArtMaster {
+    internal class BitmapToASCIIConverter {
+    }
+}
