255ca029da9876da7090366add29097e6a20ff69
Timalice1 <newacccc2@gmail.com>
2022-03-05 11:41:25 +0200

    convert bitmap to 2d array of ascii chars

--- ASCIIArtMaster/Program.cs
+++ ASCIIArtMaster/Program.cs
@@ -9,9 +9,11 @@ namespace ASCIIArtMaster {
         [STAThread]
         static void Main(string[] args) {
 
+            //Open fileDialog & filter files by img extentions 
             var openFileDilaog = new OpenFileDialog {
                 Filter = "Images | *.bmp; *.png; *.jpeg; *.JPEG; *.jpg"
             };
+
 
             while (true) {
                 Console.ReadLine();
@@ -19,19 +21,25 @@ namespace ASCIIArtMaster {
                 if (openFileDilaog.ShowDialog() != DialogResult.OK)
                     continue;
 
+                //Clear console from previous img
                 Console.Clear();
 
+                //Create new bitmap from opened file
                 var bitmap = new Bitmap(openFileDilaog.FileName);
+
+                //Resize and conver to gray scale
                 bitmap = Resize(bitmap);
                 bitmap.ToGrayScale();
 
             }
         }
 
+        //Set for opened image console size
         private static Bitmap Resize(Bitmap bitmap) {
             var maxWidth = 350;
             var newHeight = bitmap.Height/1.5 * maxWidth/bitmap.Width;
             if (bitmap.Width > maxWidth || bitmap.Height > newHeight)
+                //Resize bitmap
                 bitmap = new Bitmap(bitmap, new Size(maxWidth, (int)newHeight));
             return bitmap;
         }
